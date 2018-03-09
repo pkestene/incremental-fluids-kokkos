1cb0715145391231b7e1369d1eb54d98461427fc
Pierre Kestener <pierre.kestener@cea.fr>
2018-03-09 19:07:31 +0100

    re-introduce a body in body list

--- 4-solid-boundaries/Fluid.cpp
+++ 4-solid-boundaries/Fluid.cpp
@@ -72,7 +72,7 @@ int main(int argc, char* argv[])
   unsigned char *image = new unsigned char[sizeX*sizeY*4];
 
   SolidBodyList bodies = SolidBodyList("list of solid bodies",1);
-  //bodies(0) = SolidBody(SolidBody::BOX,0.5, 0.6, 0.7, 0.1, M_PI*0.25, 0.0, 0.0, 0.0);
+  bodies(0) = SolidBody(SolidBody::BOX,0.5, 0.6, 0.7, 0.1, M_PI*0.25, 0.0, 0.0, 0.0);
 
   
   FluidSolver *solver = new FluidSolver(sizeX, sizeY, density, bodies);
